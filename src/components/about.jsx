d3bef0606519fcb8836e513f93c383c5ea7eaf22
Emira Hajj <emirahajj@gmail.com>
2020-11-30 01:34:09 -0500

    spacing

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -8,7 +8,7 @@ export default function about() {
     <div class="justify-center">
         <img src={emoji} className="w-64 md:w-80 lg:w-96 min rounded-full" alt="Emira"/>
       </div>
-      <div class="intro max-w-xs  md:ml-6 lg:mt-6">
+      <div class="intro max-w-xs md:ml-6 lg:mt-6">
         <h1 class='text-gray-300 -ml-12 text-6xl text-left leading-12 md:-ml-16 md:text-8xl'>Hello.</h1>
         <p class="hidden text-gray-900 text-2xl leading-6 md:contents md:leading-none md:text-2xl md:mt-2 md:text-left">
         I'm a computer science 
@@ -18,7 +18,7 @@ export default function about() {
       </div>
     </div>
 
-      <div className="md:hidden max-w-xs mt-4">
+      <div className="md:hidden max-w-xs mt-3">
       <p class="text-center text-gray-600 text-xl leading-6">
         I'm a computer science 
           major at the City University of New York - Hunter College. When I'm not programming, I like to make
